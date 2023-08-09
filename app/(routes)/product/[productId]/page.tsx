b8329d9ab9a8031b700f94c33396e5c1230240be
JakubKub13 <jakubkubala3@gmail.com>
2023-08-09 07:50:13 +0200

    adding new features

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import getProducts from "@/actions/get-products";
 import Container from "@/components/ui/container";
 import ProductList from "@/components/product-list";
 import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 
 interface ProductPageProps {
     params: {
@@ -26,7 +27,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({
                         <Gallery images={product.images}/>
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
                             {/* Product info */}
-                            Info
+                            <Info data={product}/>
                         </div>
                     </div>
                 </div>
